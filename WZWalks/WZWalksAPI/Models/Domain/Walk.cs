a2269d9ff82d3d3f7c180d6086d073390c696737
MVgithub <mvacerskasgit@gmail.com>
2022-09-07 11:41:17 +0300

    End of Section 2

---
+++ WZWalks/WZWalksAPI/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace WZWalksAPI.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Leght { get; set; }
+
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //navigation props
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
--- WZWalks/WZWalksAPI/Program.cs
+++ WZWalks/WZWalksAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WZWalksAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//injection db context by me
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
